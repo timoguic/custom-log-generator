88cac4235965391872337b237e3c22f97c99374a
Tim G <timoguic@gmail.com>
2022-12-26 19:48:55 +0100

    pypi in cicd

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="dolos-log-generator",
-    version="0.1.1",
+    version="0.2.0",
     packages=find_packages(),
     include_package_data=True,
+    description="Versatile log generator with custom patterns and fields",
+    long_description=open("README.md", "r").read(),
     install_requires=[
         "click",
         "dateparser",
